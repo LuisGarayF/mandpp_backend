c0a219f20243cf7ad014cfdc9b82960208e881b4
Luis Garay Fuentes <luis.garay@defensoriacl.com>
2024-06-04 16:30:23 -0400

    conexion-oracle

--- dbconfig.js
+++ dbconfig.js
@@ -2,9 +2,9 @@
 
 module.exports = {
     user: 'SGDP_USER',
-    password: 'user_sgdp',
+    password: 'dpp123..',
     connectString: '10.0.214.9:1533/REPLICA',
-    poolMin: 10,
-    poolMax: 10,
-    poolIncrement: 0
+    //poolMin: 10,
+    //poolMax: 10,
+    //poolIncrement: 0
 };
--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const cors = require('cors');
 const dbConfig = require('./dbConfig');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Configuración para usar el driver Thick
+oracledb.initOracleClient({ libDir: 'C:\\instantclient_12_2' });
 
 // Conexión a la base de datos
 async function initialize() {
@@ -24,10 +27,16 @@ async function initialize() {
 initialize();
 
 // Rutas
-app.get('/data', async (req, res) => {
+
+app.get('/', (req, res) => {
+    res.send(`Servidor corriendo en el puerto ${PORT}`);
+  });
+
+app.get('/data/:usuprodefid', async (req, res) => {
+    const usuprodefid = req.params.usuprodefid;
     try {
         const connection = await oracledb.getConnection();
-        const result = await connection.execute('SELECT * FROM tu_tabla');
+        const result = await connection.execute('SELECT * FROM SGDP_USER.TBL_USUPRODEF WHERE USUPRODEFID = :id', [usuprodefid]);
         res.json(result.rows);
         await connection.close();
     } catch (err) {
@@ -40,3 +49,4 @@ app.get('/data', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
